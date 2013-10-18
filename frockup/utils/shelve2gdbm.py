c0e90cb75e4a07cdca2dbe16f89f76b9c80a2dd9
Horacio G. de Oro <hgdeoro@gmail.com>
2013-10-18 15:16:02 -0300

    Add shelve2gdbm

---
+++ frockup/utils/shelve2gdbm.py
@@ -0,0 +1,28 @@
+import shelve
+import gdbm
+import sys
+import os
+import json
+
+
+def main():
+    directory = sys.argv[1]
+    assert os.path.exists(directory)
+    assert os.path.isdir(directory)
+    infile = os.path.join(directory, '.frockup.db')
+    outfile = os.path.join(directory, '.frockup.gdbm')
+    assert os.path.exists(infile)
+    assert not os.path.exists(outfile)
+
+    origdb = shelve.open(infile)
+    newdb = gdbm.open(outfile, 'n')
+
+    for key, value in origdb.iteritems():
+        print " - Converting:", key
+        newdb[key] = json.dumps(value)
+
+    newdb.close()
+    origdb.close()
+
+if __name__ == '__main__':
+    main()
